b96880f489caca8c19f979c087090d3cc004db2c
Flynn-Brooks <73218015+Flynn-Brooks@users.noreply.github.com>
2023-09-28 08:41:50 +0800

    Update

---
+++ app/about-us/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <div>Meowcoin Partners</div>
+    </div>
+  );
+}
